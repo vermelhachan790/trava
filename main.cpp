01563a56b9d9b0b05c741a2a9b4d36e195f36ce8
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-04-28 15:26:35 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#include <iostream.h>
+
+
+using namespace std;
+
+int main()
+{
+
+
+cout <<"Alerta: se você tem um celular com um processador ruim, melhor não executar o programa.\n";
+cout <<"Aperte 1, para continuar\n";
+int pass;
+scanf ("%i" ,pass);
+if (pass == 1)
+{
+cout <<"Agora clique 2 para abrir a trava 'Cavalo de troia'\n";
+}
+else{
+system("clear");
+}
+
+
+
+return 0;
+
+}
 
